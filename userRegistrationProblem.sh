f0734eec5c65f119a2c22b71bdfb594df1441e69
ADMINISTRATOR <Mayankrathi33@gmail.com>
2020-02-14 19:17:34 +0530

    ADDED:Valid user email with 3 mandatory and 2 optional part

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -2,6 +2,7 @@
 echo "Welcome to user registration problem using regex"
 #constant
 CHECK_FOR_FIRSTNAME_AND_LASTNAME="^[A-Z]{1}[A-Za-z]{3,}$"
+CHECK_FOR_MAIL="^[A-Za-z]{3,}([.|_|+|-]?[a-zA-Z]+)?[@]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,3}([.]?[a-zA-Z]*)?$"
 
 #Check for valid or invalid condition
 function conditionCheck(){
@@ -22,3 +23,9 @@ echo "Enter first name and last name"
 read firstName lastName
 #For checking valid first name
 conditionCheck  $firstName $LastName$CHECK_FOR_FIRSTNAME_AND_LASTNAME
+
+echo "Enter email "
+read email 
+#Check for email validation
+conditionCheck $email $CHECK_FOR_MAIL
+
